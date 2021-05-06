fbe34900985b0f7fc0a3fe09ab02de16a1181635
codelifee <suminjung09@gmail.com>
2021-05-06 11:01:06 +0900

    feat: install necessary dependencies

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  SafeAreaView, 
+  Platform, 
+  Button, 
+  Alert,
+  Dimensions
+} from 'react-native';
+
+import {useDimentions, useDeviceOrientation} from '@react-native-community/hooks';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from "./screens/LoginScreen";
+
+
+const Stack = createStackNavigator();
+
+const globalScreenOptions = {
+  haederStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: { color : 'white' },
+  headerTintColor: "white",
+}
 
 export default function App() {
+  const {landscape} = useDeviceOrientation();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer >
+      <Stack.Navigator screenOptions={globalScreenOptions}>
+        <Stack.Screen name='Login' component={LoginScreen} />
+      </Stack.Navigator>       
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'blue',
     justifyContent: 'center',
+    alignItems: 'center'
   },
 });
---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyACZeHju5gWRs1TPGvhvuy1mdXO3jNtIhc",
+    authDomain: "signal-clone-b7e78.firebaseapp.com",
+    projectId: "signal-clone-b7e78",
+    storageBucket: "signal-clone-b7e78.appspot.com",
+    messagingSenderId: "497719639131",
+    appId: "1:497719639131:web:ca3d6ff510dc0191d3537e",
+    measurementId: "G-T6P876GQP2"
+  };
