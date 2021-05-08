80284f399e6bca912a57b9b6c77170fb5689a5a6
codelifee <suminjung09@gmail.com>
2021-05-08 13:24:25 +0900

    feat: complete register page UI

--- firebase.js
+++ firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import * as firebase from "firebase";
+import "firebase/auth";
+import "firebase/firestore";
+
 const firebaseConfig = {
     apiKey: "AIzaSyACZeHju5gWRs1TPGvhvuy1mdXO3jNtIhc",
     authDomain: "signal-clone-b7e78.firebaseapp.com",
@@ -8,3 +11,16 @@ const firebaseConfig = {
     appId: "1:497719639131:web:ca3d6ff510dc0191d3537e",
     measurementId: "G-T6P876GQP2"
   };
+
+let app;
+
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
+}
+
+const db = app.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { KeyboardAvoidingView } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
 import {Button, Input, Image} from 'react-native-elements'
+import { auth } from '../firebase'
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((authUser) => {
+            if(authUser) {
+                navigation.replace("Home");
+            }
+        });
+
+        return unsubscribe;
+    }, [])
+
+    const signIn = () => {
+        
+    }
+
     return (
-        <View>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
             <StatusBar style="light" />
             <Image 
             source={{
@@ -15,18 +34,47 @@ const LoginScreen = () => {
             style={{width: 200, height: 200}}
             />
             <View style={styles.inputContainer}>
-                <Input placeholder="Email" autoFocus type="Email" />
-                <Input placeholder="Password" secureTextEntry type="Password" />
+                <Input 
+                placeholder="Email" 
+                autoFocus type="Email" 
+                value={email}
+                onChangeText={text => setEmail(text)}
+                />
+                <Input 
+                placeholder="Password" 
+                secureTextEntry 
+                type="password" 
+                value={password}
+                onChangeText={text => setPassword(text)}
+                />
             </View>
-        </View>
+
+            <Button containerStyle={styles.button}
+            onPress={signIn} title="Login" />
+            <Button containerStyle={styles.button} 
+            onPress={() => navigation.navigate('Register')}
+            type="outline" title="Register" />
+            <View style={{ height: 100 }}/>
+        </KeyboardAvoidingView>
     )
 }
 
 export default LoginScreen
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        backgroundColor: "white"
+    },
     inputContainer: {
-        
+        width: 300
+    },
+    button: {
+        width: 200,
+        marginTop: 10
     }
 })
 
