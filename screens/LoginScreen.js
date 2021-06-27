e568ed2a80a0c1da27946c6da373f556750979ab
suminjung <suminjung@boomco.org>
2021-06-27 22:03:35 +0900

    feat: complete LoginScreen component

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import {Button, Input, Image} from 'react-native-elements'
 
 const LoginScreen = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
-        <View>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
             <StatusBar style="light" />
             <Image 
             source={{
@@ -15,18 +19,46 @@ const LoginScreen = () => {
             style={{width: 200, height: 200}}
             />
             <View style={styles.inputContainer}>
-                <Input placeholder="Email" autoFocus type="Email" />
-                <Input placeholder="Password" secureTextEntry type="Password" />
+                <Input 
+                placeholder="Email" 
+                autoFocus 
+                type="Email" 
+                onChangeText={(text) => setEmail(text)}
+                />
+
+                <Input 
+                placeholder="Password" 
+                secureTextEntry 
+                type="Password" 
+                onChangeText={(text) => setPassword(text)}
+                />
+                
             </View>
-        </View>
+
+            <Button containerStyle={styles.button} onPress={} title="Login"/>
+            <Button containerStyle={styles.button} onPress={} title="Login"/>
+            <View style={{ height: 100 }} />
+        </KeyboardAvoidingView>
     )
 }
 
 export default LoginScreen
 
 const styles = StyleSheet.create({
+    container: 
+    { 
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+        backgroundColor: "white"
+    },
     inputContainer: {
-        
+       width: 300
+    },
+    button: {
+        width: 200,
+        marginTop: 10,
     }
 })
 
